5ac8174c064fffc6a9ecdfaa40f35b1bddc685f5
MartinKearney <martin-kearney@hotmail.co.uk>
2020-07-04 05:24:15 +0100

    Updates

---
+++ precache-manifest.0f253b062f8829b64e2116bcfa38e8ad.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e9c4d08eafdfb3aeef8827b4b003d98",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "4e23e3747dfcbd31e6c4",
+    "url": "/Portfolio/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "23edfcaf64c0dc9c71a7",
+    "url": "/Portfolio/static/css/main.59ffeea8.chunk.css"
+  },
+  {
+    "revision": "4e23e3747dfcbd31e6c4",
+    "url": "/Portfolio/static/js/2.7d185913.chunk.js"
+  },
+  {
+    "revision": "d8b4173f7a8b6c66c369ccd5944b6a3e",
+    "url": "/Portfolio/static/js/2.7d185913.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23edfcaf64c0dc9c71a7",
+    "url": "/Portfolio/static/js/main.ff7498ef.chunk.js"
+  },
+  {
+    "revision": "627bdb01d220a474fd8b",
+    "url": "/Portfolio/static/js/runtime-main.9e287cfb.js"
+  },
+  {
+    "revision": "7b3e9f73277f695a05332554dc3708aa",
+    "url": "/Portfolio/static/media/logger.7b3e9f73.png"
+  },
+  {
+    "revision": "a8697b1981eb06acff728b61c13b8ee0",
+    "url": "/Portfolio/static/media/url-shortener.a8697b19.png"
+  },
+  {
+    "revision": "8455a0221381ca6f5af09c1806c1139d",
+    "url": "/Portfolio/static/media/weather1.8455a022.png"
+  }
+]);
